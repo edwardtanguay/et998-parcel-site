fc3e0e896d575c40f47b87e8c8652ee7f2d5e315
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:50:57 +0200

    index in map

--- src/components/Skills.ts
+++ src/components/Skills.ts
@@ -4,9 +4,9 @@ export const Skills = (skills:ISkill[]) => {
 	return /*html*/ `
 		<h2 class="text-2xl mt-3">There are ${skills.length} skills.</h2>
 		<ul>
-		${skills.map(skill => {
+		${skills.map((skill,index) => {
 			return /*html*/ `
-				<li><span class="font-bold">${skill.name}</span> - ${skill.description}</li>	
+				<li>${index + 1}. <span class="font-bold">${skill.name}</span> - ${skill.description}</li>	
 			`
 		}).join('')}
 		</ul>
