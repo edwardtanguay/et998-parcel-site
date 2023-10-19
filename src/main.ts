b2fd878811847a3397e0eb856cc0619b3d9449ec
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-10-19 14:23:18 +0200

    added 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.scss";
 
 document.querySelector("#app")!.innerHTML = /*html*/ `
-  <h1 class="text-blue-950 text-4xl">Parcel Site</h1>
+  <h1 class="text-blue-950 text-4xl">Parcel Site 2</h1>
 `;
