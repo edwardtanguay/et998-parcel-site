69ac5baf82266c7fcfc1c4294b38a781dcc8ec63
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:05:33 +0200

    added image

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.scss';
 
 document.querySelector('#app')!.innerHTML  = /*html*/ `
-	<h1 class="text-blue-950 text-4xl">Parcel Site</h1>
+	<h1 class="text-black text-4xl mb-3">Parcel Site</h1>
+	<img class="rounded" src="images/code.png"/>
 `;
