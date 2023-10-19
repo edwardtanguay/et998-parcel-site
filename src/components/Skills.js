09cf688160ee9c9a3f892b0c8c1269b3ca1f7df8
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:39:17 +0200

    skills

---
+++ src/components/Skills.js
@@ -0,0 +1,5 @@
+export const Skills = (skills) => {
+	return /*html*/ `
+		<h2 class="text-2xl mt-3">There are ${skills.length} skills.</h2>
+	`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
 import './style.scss';
+import skills from '../data/skills.json';
 
 document.querySelector('#app')!.innerHTML  = /*html*/ `
 	${Header()}
 	<img class="rounded" src="images/code.png"/>
+	${Skills()}
 `;
