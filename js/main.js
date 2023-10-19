68935f53d36e8877f1b25e1e4d42115ecfde181b
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 11:02:06 +0200

    implemented ES6 modules

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('test');
+import { changeTitle } from "./tools.js";
 
-const h1Elem = document.querySelector('h1');
-h1Elem.style = "color:red; background-color: yellow"
+console.log('test');
+changeTitle();
---
+++ js/tools.js
@@ -0,0 +1,4 @@
+export const changeTitle = () => {
+	const h1Elem = document.querySelector('h1');
+	h1Elem.style = "color:red; background-color: yellow";
+};
