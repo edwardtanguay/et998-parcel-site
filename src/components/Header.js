c8a54bc36bc7dae7e6bcd1458e8f81e84700e333
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:10:54 +0200

    components in

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export const Header = () => {
+	return /*html*/ `
+		<h1 class="text-black text-4xl mb-3">Parcel Site</h1>
+	`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { Header } from './components/Header';
 import './style.scss';
 
 document.querySelector('#app')!.innerHTML  = /*html*/ `
-	<h1 class="text-black text-4xl mb-3">Parcel Site</h1>
+	${Header()}
 	<img class="rounded" src="images/code.png"/>
 `;
