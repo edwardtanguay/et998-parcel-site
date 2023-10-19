9d26c9874cb53633bdfe142edece8837408694a1
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:45:34 +0200

    skills with interfaces and tsconfig.json

--- src/components/Skills.ts
+++ src/components/Skills.ts
@@ -1,4 +1,6 @@
-export const Skills = (skills) => {
+import { ISkill } from "../interfaces";
+
+export const Skills = (skills:ISkill[]) => {
 	return /*html*/ `
 		<h2 class="text-2xl mt-3">There are ${skills.length} skills.</h2>
 	`;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { Header } from './components/Header';
 import { Skills } from './components/Skills';
 import './style.scss';
-import skills from '../data/skills.json';
+import skills from './data/skills.json';
 
 document.querySelector('#app')!.innerHTML  = /*html*/ `
 	${Header()}
 	<img class="rounded" src="images/code.png"/>
-	${Skills()}
+	${Skills(skills)}
 `;
