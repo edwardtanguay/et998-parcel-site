83dbaa62b81ddc11892ba567309948e3c7e2c5b5
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-10-19 14:08:21 +0200

    changed color

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.scss";
 
 document.querySelector("#app")!.innerHTML = /*html*/ `
-  <h1 class="text-blue-950 bg-red-500 text-4xl">Parcel Site</h1>
+  <h1 class="text-blue-950 text-4xl">Parcel Site</h1>
 `;
