939a79c6fa91987b135356e5e541e26875fc3a0d
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-19 15:48:03 +0200

    skills

--- src/components/Skills.ts
+++ src/components/Skills.ts
@@ -3,5 +3,12 @@ import { ISkill } from "../interfaces";
 export const Skills = (skills:ISkill[]) => {
 	return /*html*/ `
 		<h2 class="text-2xl mt-3">There are ${skills.length} skills.</h2>
+		<ul>
+		${skills.map(skill => {
+			return /*html*/ `
+				<li><span class="font-bold">${skill.name}</span> - ${skill.description}</li>	
+			`
+		}).join('')}
+		</ul>
 	`;
 }
